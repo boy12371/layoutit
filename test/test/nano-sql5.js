681daf2375e404224e8ec32948f7ac72b045161a
richard <1607409@qq.com>
2018-05-26 19:16:17 +0800

    20180526191505

--- test/test/nano-sql5.js
+++ test/test/nano-sql5.js
@@ -1,5 +1,5 @@
-load1();
-load3();
+// load1();
+// load3();
 
 async function load1() {
   await new Promise((resolve, reject) => {
@@ -10,14 +10,14 @@ async function load1() {
   });
 }
 
-(function load2() {
-  console.log("代码2执行完毕");
-})();
+// (function load2() {
+//   console.log("代码2执行完毕");
+// })();
 
-let resolved = new Promise((resolve, reject) => {
-  (resolve, reject) => resolve("foo");
-});
-resolved.then(str => console.log(str));
+// let resolved = new Promise((resolve, reject) => {
+//   (resolve, reject) => resolve("foo");
+// });
+// resolved.then(str => console.log(str));
 
 async function load3() {
   await new Promise((resolve, reject) => {
@@ -34,3 +34,25 @@ async function load3() {
       console.log(error, "这里捕获Promise错误");
     });
 }
+
+// load4().then(alert => console.log(alert));
+
+async function load4() {
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("done!"), 1000);
+  });
+
+  return await promise;
+  // return Promise.resolve(result);
+  // return await Promise.resolve(promise);
+}
+
+function a(data) {
+  return data;
+}
+
+function b(fn) {
+  var b = fn;
+  console.log(b);
+}
+b(a("aaaaa"));
